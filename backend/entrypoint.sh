8090042e596f19b9852e55b02e1a1ed177b198c0
Akash Shinde <saakashs1999@gmail.com>
2024-07-07 16:52:07 +0530

    updated the Dockerfiles

---
+++ backend/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Wait for the WAR file to be extracted
+while [ ! -f /usr/local/tomcat/webapps/spring-backend/WEB-INF/classes/application.properties ]; do
+    sleep 1
+done
+
+# Modify the application.properties file
+sed -i 's/spring.datasource.url=jdbc:mysql:\/\/localhost:3306\/springbackend?useSSL=false/spring.datasource.url=jdbc:mysql:\/\/database-1.cru2a806a2xh.us-east-1.rds.amazonaws.com:3306\/springbackend?useSSL=false/' /usr/local/tomcat/webapps/spring-backend/WEB-INF/classes/application.properties
+sed -i 's/spring.datasource.username=springbackend/spring.datasource.username=admin/' /usr/local/tomcat/webapps/spring-backend/WEB-INF/classes/application.properties
+sed -i 's/spring.datasource.password=springbackend/spring.datasource.password=akash123/' /usr/local/tomcat/webapps/spring-backend/WEB-INF/classes/application.properties
+
+echo "" >> /usr/local/tomcat/webapps/spring-backend/WEB-INF/classes/application.properties
+echo 'server.port=8085' >> /usr/local/tomcat/webapps/spring-backend/WEB-INF/classes/application.properties
+
+# Start Tomcat
+catalina.sh run
