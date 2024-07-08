0b4605cacee68dfe8a34af1e732e1ed8b5aa60d3
Akash Shinde <saakashs1999@gmail.com>
2024-07-08 17:25:22 +0530

    resolved some errors

--- springbackend.sql
+++ springbackend.sql
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `tbl_workers`
 --
 CREATE Database springbackend;
-USE Database springbackend;
+use springbackend;
 CREATE TABLE `tbl_workers` (
   `id` bigint(20) NOT NULL,
   `status` varchar(255) DEFAULT NULL,
